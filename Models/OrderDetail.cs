efad3d33ee82fb2b4e7268eb91e3f65c7eca3766
Mike Kistler (from Dev Box) <mikekistler@microsoft.com>
2023-10-30 08:48:44 -0500

    Add models

---
+++ Models/OrderDetail.cs
@@ -0,0 +1,13 @@
+﻿namespace ContosoPizza.Models;
+
+// Represents the many-to-many relationship between Order and Product
+// Teddy Smith's convention would name this class OrderProduct
+public class OrderDetail
+{
+    public int Id { get; set; }
+    public int OrderId { get; set; }
+    public int ProductId { get; set; }
+    public int Amount { get; set; }
+    public Order Order { get; set; } = null!;
+    public Product Product { get; set; } = null!;
+}
